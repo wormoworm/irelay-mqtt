681e9f100a2b74dacf523c2880e67ac16e12c45e
Tom Homewood <tomhomewood@gmail.com>
2023-01-14 16:29:44 +0000

    Removed dead creds.

--- scripts/run-publisher-poetry.sh
+++ scripts/run-publisher-poetry.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-export MQTT_ENDPOINT=10.0.1.2
-export MQTT_PORT=8884
+export MQTT_ENDPOINT=TODO
+export MQTT_PORT=TODO
 export MQTT_CLIENT_ID=irelay_publisher
 export DEBUG=True
 
-export HTTP_ENDPOINT=community.grainfather.com
-export HTTP_PATH_ISPINDEL_CHANNEL_1=/iot/feat-shit/ispindel
+export HTTP_ENDPOINT=TODO
+export HTTP_PATH_ISPINDEL_CHANNEL_1=TODO
 export HTTP_PATH_ISPINDEL_CHANNEL_2=TODO
-export HTTP_PATH_NAUTILIS=/iot/rate-lame/custom
+export HTTP_PATH_NAUTILIS=TODO
 
 poetry run python -u ../src/publisher.py
